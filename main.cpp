7c03344f5cb4c55312ee1e2d06ebc79fd5d088fc
Lev Fomenko <flevvi@gmail.com>
2020-02-09 00:50:51 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -2,38 +2,110 @@
 #include "message_bus.h"
 
 using namespace std;
+using namespace chrono_literals;
 
 int main()
 {
     MessageBus bus;
 
-    auto fut = bus.add_order<int>();
-    bus.add_order<float>();
+    {
+        std::promise<int> p;
 
-    auto fut3 = bus.add_order<std::string>();
+        {
+            p.get_future();
+            //p.get_future(); Error std::future_error: Future already retrieved
+        }
 
-    cout << "size = " << sizeof (bus) << "\n";
+        auto thrd = std::thread( [p = std::move(p)]() mutable {
+            this_thread::sleep_for(100ms);
+            p.set_value(10);
+            //p.get_future(); Error std::future_error: Future already retrieved
+            cout << "OK" << endl;
+        } );
 
-    auto t2 = std::thread( [&]{ bus.send(float(10));
+        thrd.join();
+    }
 
-        cout << "t2 OK \n";
+    {
+        std::future<int> f;
+        {
+            std::promise<int> p;
+            f = p.get_future();
+            cout << (f.valid() ? "future valid" : "future unvalid") << endl;
+        }
+        cout << (f.valid() ? "future valid" : "future unvalid") << endl;
+        //f.get(); Error std::future_error: Broken promise
+    }
 
-    } );
+    auto check_subscriber_simple = [&bus](auto value) {
+        using type_t = typename std::remove_reference<decltype(value)>::type;
+        auto fut = bus.add_order<type_t>();
 
-    auto t3 = std::thread( [&]{ bus.send(std::string("OKKK!!!!"));
+        cout << "value = " << value  << "\n";
 
-        cout << "t2 OK \n";
+        cout << "subs count = " << bus.orders_count()  << "\n";
 
-    } );
+        auto thrd = std::thread( [&]{
+            this_thread::sleep_for(100ms);
+            //bus.send(value); //TODO only move works well
+            bus.send(std::move(value));
+        } );
 
-    auto t = std::thread( [&]{ bus.send(int(10)); } );
+        bool res = fut.wait_for(500ms);
 
-    auto res = fut.get();
-    t.join();
-    t2.join();
-    t3.join();
-    cout << "res = " << ((Message<int>*)res.get())->payload << "\n";
-    cout << "res = " << ((Message<std::string>*)fut3.get().get())->payload << "\n";
+        cout << "wait_for result =" << res << "\n";
+
+        cout << "subs count = " << bus.orders_count()  << "\n";
+
+        cout << "result =" << fut.get()->payload << "\n";
+
+        thrd.join();
+
+        cout << "==============================\n";
+    };
+
+
+    check_subscriber_simple(std::string("TEST1"));
+    check_subscriber_simple(41);
+    check_subscriber_simple(42l);
+    check_subscriber_simple(43.0);
+
+
+    cout << "==============================\n";
+    cout << "==============================\n";
+    cout << "==============================\n";
+
+    auto check_subscriber_simple2 = [&bus](auto value) {
+        using type_t = typename std::remove_reference<decltype(value)>::type;
+        auto fut = bus.add_order<type_t>();
+
+        cout << "value = " << value  << "\n";
+
+        cout << "subs count = " << bus.orders_count()  << "\n";
+
+        auto thrd = std::thread( [&]{
+            this_thread::sleep_for(100ms);
+            bus.send(std::move(value));
+        } );
+
+        type_t v {};
+        bool res = fut.wait_for(500ms, v);
+
+        cout << "wait_for result =" << res << "\n";
+
+        cout << "subs count = " << bus.orders_count()  << "\n";
+
+        cout << "result =" << v << "\n";
+
+        thrd.join();
+
+        cout << "==============================\n";
+    };
+
+    check_subscriber_simple2(std::string("TEST1"));
+    check_subscriber_simple2(343434);
+    check_subscriber_simple2(4444l);
+    check_subscriber_simple2(5555.0);
 
     return 0;
 }
--- message_bus.h
+++ message_bus.h
@@ -18,7 +18,7 @@ constexpr mes_id_t gen_mes_id(mes_id_t id = 0)
 
 
 struct MessageI {
-    virtual ~MessageI() {};
+    virtual ~MessageI() = default;
 };
 
 
@@ -26,35 +26,66 @@ template<class T>
 struct Message : MessageI {
     using payload_type = T;
 
-    Message(T&& v) : payload(std::forward<T>(v)) {
+    //Message(T&& v) : payload(std::forward<T>(v)) { }      TODO not worked well
 
+    Message(T&& v) {
+        payload = std::forward<T>(v);
     }
 
     T payload;
-
-    ~Message() {}
 };
+
 
 
 class MessageBus {
     using Promise_t = std::promise<std::unique_ptr<MessageI>>;
+    using Future_t = std::future<std::unique_ptr<MessageI>>;
 
     std::mutex mtx;
 
     std::vector<std::pair<mes_id_t, Promise_t>> consumers;
 
-
 public:
+    template<class T>
+    struct FutureWrapper {
+        Future_t fut;
+
+        FutureWrapper(Future_t&& f) : fut(std::forward<Future_t>(f)) {}
+
+        template<class Duration = std::chrono::microseconds>
+        bool wait_for(Duration period, T& value) {
+            if (fut.wait_for(period) == std::future_status::ready) {
+                if (auto ptr = fut.get()) {
+                    value = std::move(static_cast<Message<T>*>(ptr.get())->payload);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        template<class Duration = std::chrono::microseconds>
+        bool wait_for(Duration period) {
+            if (fut.wait_for(period) == std::future_status::ready) {
+                return true;
+            }
+            return false;
+        }
+
+        std::unique_ptr<Message<T>> get() {
+            auto ptr = fut.get();
+            return std::unique_ptr<Message<T>>( static_cast<Message<T>*>(ptr.release()) );
+        }
+    };
+
+
     template<class T, mes_id_t id = 0>
-    std::future<std::unique_ptr<MessageI>> add_order() {
+    FutureWrapper<T> add_order() {
         Promise_t prom;
-        auto fut = prom.get_future();
+        auto fut = FutureWrapper<T>( prom.get_future() );
         std::lock_guard<std::mutex> lock(mtx);
         consumers.push_back( std::make_pair( gen_mes_id<T>(id), std::move(prom) ) );
         return fut;
     }
-
-
 
     template<class T>
     bool send(T&& value, mes_id_t id = 0) {
@@ -63,14 +94,20 @@ public:
         const auto expected_id = gen_mes_id<T>(id);
         for(auto el = consumers.begin() ; el != consumers.end() ; el++ ) {
             if (el->first == expected_id) {
+                auto ptr = new Message<T>(std::forward<T>(value));
                 el->second.set_value( std::unique_ptr<MessageI>(
-                                          static_cast<MessageI*>( new Message<T>( std::forward<T>(value) ) ) ) );
+                                          static_cast<MessageI*>( ptr ) ));
                 result = true;
                 consumers.erase(el);
                 break;
             }
         }
         return result;
+    }
+
+    auto orders_count() {
+        std::lock_guard<std::mutex> lock(mtx);
+        return consumers.size();
     }
 };
 
